0dc94647f65c8b711b1a3847f888c4b2fb69baab
martin-b-07e5 <martin.bergagno@gmail.com>
2024-11-01 11:32:35 -0300

    top 10 books done

--- src/main/java/com/example/gutendexBooks/main/Main.java
+++ src/main/java/com/example/gutendexBooks/main/Main.java
@@ -1,31 +1,33 @@
 package com.example.gutendexBooks.main;
 
+import com.example.gutendexBooks.model.Datos;
 import com.example.gutendexBooks.service.ConsumoAPI;
+import com.example.gutendexBooks.service.ConvierteDatos;
 import com.example.gutendexBooks.util.BuscarPorTitulo;
+import com.example.gutendexBooks.util.TopTenDownloads;
 
 import java.util.Scanner;
 
-import static java.lang.Thread.sleep;
-
 public class Main {
+
+  Scanner scanner = new Scanner(System.in);
+  String URL_BASE = "https://gutendex.com/books/";
+  String URL_SEARCH = "http://gutendex.com/books/?search=";
+  /*search
+    Use this to search author names and book titles with given words.
+     They must be separated by a space (i.e. %20 in URL-encoded format) and are case-insensitive.
+      For example, /books?search=dickens%20great includes Great Expectations by Charles Dickens.*/
+  ConsumoAPI consumoAPI = new ConsumoAPI();
+  ConvierteDatos conversor = new ConvierteDatos();
 
   // Method
   public void muestraElMenu() {
 
-    Scanner scanner = new Scanner(System.in);
-    ConsumoAPI consumoAPI = new ConsumoAPI();
-//    String URL_BASE = "https://gutendex.com/books/";
-    String URL_SEARCH = "http://gutendex.com/books/?search=";
-    /*search
-    Use this to search author names and book titles with given words.
-     They must be separated by a space (i.e. %20 in URL-encoded format) and are case-insensitive.
-      For example, /books?search=dickens%20great includes Great Expectations by Charles Dickens.*/
-
     int option = 0;
     while (option != 3) {
-      System.out.println("Bienvenido a Gutendex Books!");
+      System.out.println("\nBienvenido a Gutendex Books!");
       System.out.println("1. Buscar libros por título");
-      System.out.println("2. top 10 libros más descargados");
+      System.out.println("2. Top 10 libros más descargados");
       System.out.println("3. Salir");
       System.out.print("Elija una opción: ");
 
@@ -41,7 +43,17 @@ public class Main {
           BuscarPorTitulo.buscarPorTitulo(scanner, consumoAPI, URL_SEARCH);
           break;
         case 2:
-//          2. top 10 libros más descargados
+//          2. Top 10 libros más descargados
+          System.out.println("------------------------------");
+          var json = consumoAPI.obtenerDatos(URL_BASE);
+          // Obtiene los datos generales de los LIBROS
+          System.out.println("json = " + json);
+
+//          var datos = conversor.obtenerDatosx(json, Datos.class);
+          Datos datos = conversor.obtenerDatosx(json, Datos.class);
+          System.out.println("\ndatos = " + datos);
+          System.out.println("------------------------------");
+          TopTenDownloads.mostrarTop10LibrosMasDescargados(datos.results());
           break;
         case 3:
           System.out.println("Gracias por usar Gutendex Books!");
@@ -50,9 +62,14 @@ public class Main {
           System.out.println("Opción inválida. Por favor, elija una opción válida.");
       }
 
-
     }
+
+    scanner.close(); // Close the scanner object
+
   }
 
 
 }
+
+
+
---
+++ src/main/java/com/example/gutendexBooks/util/TopTenDownloads.java
@@ -0,0 +1,32 @@
+package com.example.gutendexBooks.util;
+
+import com.example.gutendexBooks.model.DatosLibros;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class TopTenDownloads {
+
+  public static void mostrarTop10LibrosMasDescargados(List<DatosLibros> libros) {
+
+    // Top 10 libros más descargados
+    System.out.println("Top 10 libros más descargados");
+//    datos.results().stream()
+    libros.stream()
+        .sorted(Comparator
+            .comparing(DatosLibros::download_count)
+            .reversed())
+        .limit(10)
+//        .peek(System.out::println)  // imprime la primer parte unformatted
+        .map(datosLibros -> String.format("Title: %s, Author: %s, Birth_year; %d, Language: %s, Downloads: %d",
+                datosLibros.title(),
+                datosLibros.authors().getFirst().getName(),
+                datosLibros.authors().getFirst().getBirth_year(),
+                datosLibros.languages().getFirst().toUpperCase(),
+                datosLibros.download_count()
+            )
+        )
+
+        .forEach(System.out::println);
+  }
+}
